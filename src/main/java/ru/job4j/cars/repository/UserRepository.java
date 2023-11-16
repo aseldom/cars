3c29a4099845fe1f10e90a9c409f7aebc71f44eb
aseldom <a.seldom@gmail.com>
2023-11-16 17:31:06 +0600

    2. HQL [#6874]

---
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -0,0 +1,129 @@
+package ru.job4j.cars.repository;
+
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import ru.job4j.cars.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class UserRepository {
+    private final SessionFactory sf;
+
+    /**
+     * Сохранить в базе.
+     * @param user пользователь.
+     * @return пользователь с id.
+     */
+    public User create(User user) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return user;
+    }
+
+    /**
+     * Обновить в базе пользователя.
+     * @param user пользователь.
+     */
+    public void update(User user) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "UPDATE User SET password = :fPassword WHERE login = :fLogin")
+                    .setParameter("fLogin", user.getLogin())
+                    .setParameter("fPassword", user.getPassword())
+                    .executeUpdate();
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+    }
+
+    /**
+     * Удалить пользователя по id.
+     * @param userId ID
+     */
+    public void delete(int userId) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "DELETE FROM User WHERE id = :fId")
+                    .setParameter("fId", userId)
+                    .executeUpdate();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+
+    }
+
+    /**
+     * Список пользователь отсортированных по id.
+     * @return список пользователей.
+     */
+    public List<User> findAllOrderById() {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        List<User> result = session.createQuery("FROM User u ORDER BY u.id", User.class).list();
+        session.getTransaction().commit();
+        session.close();
+        return result;
+    }
+
+    /**
+     * Найти пользователя по ID
+     * @return пользователь.
+     */
+    public Optional<User> findById(int userId) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        User result = session.get(User.class, userId);
+        session.getTransaction().commit();
+        session.close();
+        return Optional.ofNullable(result);
+    }
+
+    /**
+     * Список пользователей по login LIKE %key%
+     * @param key key
+     * @return список пользователей.
+     */
+    public List<User> findByLikeLogin(String key) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        List<User> result = session.createQuery("FROM User WHERE login LIKE :fKey", User.class)
+                .setParameter("fKey", "%" + key + "%")
+                .list();
+        session.getTransaction().commit();
+        session.close();
+        return result;
+    }
+
+    /**
+     * Найти пользователя по login.
+     * @param login login.
+     * @return Optional or user.
+     */
+    public Optional<User> findByLogin(String login) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        User result = session.createQuery("FROM User WHERE login = :fLogin", User.class)
+                .setParameter("fLogin", login)
+                .uniqueResult();
+        session.getTransaction().commit();
+        session.close();
+        return Optional.ofNullable(result);
+    }
+}
