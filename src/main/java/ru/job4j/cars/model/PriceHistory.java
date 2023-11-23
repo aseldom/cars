9127c07777915f45356fdc1873b5743f4635b82b
aseldom <a.seldom@gmail.com>
2023-11-23 18:21:19 +0600

    1. ToMany [#301848]

---
+++ src/main/java/ru/job4j/cars/model/PriceHistory.java
@@ -0,0 +1,26 @@
+package ru.job4j.cars.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "price_history")
+public class PriceHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+    private Long before;
+    private Long after;
+    private Timestamp startTime;
+}
